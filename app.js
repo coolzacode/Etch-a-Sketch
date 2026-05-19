345d2f74d0855389ef25d40c44fe9a0beb601058
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-05-19 15:40:09 -0500

    feat: make controls functionable

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById('canvas');
+const sizeSlider = document.getElementById('size-slider');
+const sizeValue = document.getElementById('size-value');
+const resetBtn = document.getElementById('reset-btn');
+
+let isDrawing = false;
 
 function makeGrid(size = 16) {
+    canvas.replaceChildren();
     canvas.style.setProperty('--grid-size', size);
+
     const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < size * size; i++) {
@@ -12,16 +19,29 @@ function makeGrid(size = 16) {
     canvas.appendChild(fragment);
 }
 
-function addEventListeners() {
-    canvas.addEventListener('mouseover', (e) => {
-        if (e.target.classList.contains('active-pixel')) return;
+function handleDrawing(e) {
+    if (e.type === 'mouseover' && !isDrawing) return;
 
-        const activePixel = e.target.closest('div'); 
-        if (!activePixel) return;
-
-        activePixel.classList.toggle('active-pixel');
-    })
+    if (e.target.classList.contains('pixel')) {
+        e.target.classList.add('active-pixel');
+    }
 }
 
+window.addEventListener('mousedown', () => isDrawing = true);
+window.addEventListener('mouseup', () => isDrawing = false);
+
+canvas.addEventListener('mouseover', handleDrawing);
+canvas.addEventListener('mousedown', handleDrawing);
+
+sizeSlider.addEventListener('input', (e) => {
+    const val = e.target.value;
+    sizeValue.textContent = `${val} x ${val}`;
+    makeGrid(val);
+});
+
+resetBtn.addEventListener('click', () => {
+    makeGrid(sizeSlider.value);
+})
+
+
 makeGrid();
-addEventListeners();
