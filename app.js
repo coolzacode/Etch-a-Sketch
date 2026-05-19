ffbdab40820c7da48b48ef3321fa1dd968ba5d14
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-05-19 13:40:03 -0500

    feat: add event listeners to change colors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('canvas');
 
-function makeGrid(rowSize = 4) {
+function makeGrid(rowSize = 16) {
     const gridSize = rowSize * rowSize;
     for (let i = 0; i < gridSize; i++) {
         const newDiv = document.createElement('div');
@@ -12,4 +12,17 @@ function makeGrid(rowSize = 4) {
     }
 }
 
+function addEventListeners() {
+    canvas.addEventListener('mouseover', (e) => {
+        if (e.target.classList.contains('active-pixel')) return;
+
+        const activePixel = e.target.closest('div'); 
+        if (!activePixel) return;
+
+        activePixel.classList.toggle('active-pixel');
+        console.log("hi");
+    })
+}
+
 makeGrid();
+addEventListeners();
